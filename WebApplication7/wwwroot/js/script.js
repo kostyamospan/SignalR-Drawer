855ab5b6c4ff0acb857103a142abe3390f19258d
kostya.mospan <kostya04042001@gmail.com>
2021-03-14 13:44:28 +0200

    cell drawing added

--- WebApplication7/wwwroot/js/script.js
+++ WebApplication7/wwwroot/js/script.js
@@ -1,4 +1,59 @@
 ﻿
+
+class Canvas {
+    constructor(canvasDOM, canvasSideSize, xyLinesCount) {
+        this.canvasDOM = canvasDOM;
+
+        this.canvasDOM.width = canvasSideSize;
+        this.canvasDOM.height = canvasSideSize;
+
+        this.context = canvasDOM.getContext("2d");
+        this.canvasSideSize = canvasSideSize;
+
+        this.cellSize = canvasSideSize / xyLinesCount;
+
+        this.linesCount = xyLinesCount;
+    }
+
+    drawPoint(xAbs, yAbs, color) {
+        this.context.fillStyle = color.toRGBAString();
+
+        let canvasXPos = Math.trunc((xAbs * this.canvasDOM.width / this.canvasDOM.clientWidth) / this.cellSize);
+        let canvasYPos = Math.trunc((yAbs * this.canvasDOM.height / this.canvasDOM.clientHeight) / this.cellSize);
+
+
+        this.context.fillRect(
+            canvasXPos * this.cellSize,
+            canvasYPos * this.cellSize,
+            this.cellSize,
+            this.cellSize
+        );
+    }
+
+
+    canvasLineUp() {
+        let step = this.canvasSideSize / this.linesCount;
+
+        for (let x = 0; x < this.canvasSideSize; x += step) {
+            this.context.beginPath();
+            this.context.moveTo(x, 0);
+            this.context.lineTo(x, this.canvasSideSize);
+            this.context.stroke();
+        }
+        for (let y = 0; y < this.canvasSideSize; y += step) {
+            this.context.beginPath();
+            this.context.moveTo(0, y);
+            this.context.lineTo(this.canvasSideSize, y);
+            this.context.stroke();
+        }
+    }
+
+    clear() {
+        this.context.clearRect(0, 0, this.canvasDOM.width, this.canvasDOM.height);
+        this.canvasLineUp();
+    }
+}
+
 function Color(r, g, b, a) {
     this.r = r;
     this.g = g;
@@ -10,28 +65,14 @@ function Color(r, g, b, a) {
 
 let toRGBAStringMixin = {
     toRGBAString() {
-        return `rgba(${this.r},${this.g},${this.b},${this.a})`; 
+        return `rgba(${this.r},${this.g},${this.b},${this.a})`;
     }
 };
 
 
-function getCanvas(canvasId, width = 500, height = 500) {
+function getCanvas(canvasId, cellSize = 1000) {
     let canvas = document.getElementById(canvasId);
-
-    canvas.width = width;
-    canvas.height = height;
-
-    return {
-        canvasDOM: canvas,
-        context: canvas.getContext("2d"),
-        drawPoint(x, y, color) {
-            this.context.fillStyle = color.toRGBAString();
-            this.context.fillRect(x, y, 10, 10);
-        },
-        clear() {
-            this.context.clearRect(0, 0, canvas.width, canvas.height);
-        }
-    };
+    return new Canvas(canvas, 1000, 25);
 }
 
 
@@ -42,6 +83,9 @@ function hubSetup(hubConn) {
         canvas.drawPoint(desPoint.x, desPoint.y, desPoint.color);
     });
 }
+
+
+
 
 const DEFAULT_FILL_COLOR = new Color(0, 0, 0, 1);
 
@@ -57,6 +101,8 @@ hubConn.start();
 let canvas = getCanvas("draw-canvas");
 let colorPicker = document.getElementById("color-picker");
 let colorOpacity = document.getElementById("color-opacity");
+
+canvas.canvasLineUp(canvas, 30, 30);
 
 colorPicker.addEventListener("change", (ev) => {
     let color = colorPicker.value;
@@ -80,10 +126,12 @@ document.getElementById("clear-btn").addEventListener("click", (ev) => {
 canvas.canvasDOM.onclick = function (event) {
     let bound = canvas.canvasDOM.getBoundingClientRect();
 
-    let x = event.clientX - bound.left //- canvas.clientLeft;
-    let y = event.clientY - bound.top //- canvas.clientTop;
+    let x = event.clientX - bound.left - canvas.canvasDOM.clientLeft;
+    let y = event.clientY - bound.top - canvas.canvasDOM.clientTop;
 
     canvas.drawPoint(x, y, currentFillColor);
 
     hubConn.invoke("AddPoint", JSON.stringify({ x: x, y: y, color: currentFillColor }));
 };
+
+
